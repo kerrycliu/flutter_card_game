acf3c861b7d703f976652a59355466b55bc55f92
KleinS <freaKENamazing18@gmail.com>
2024-03-21 11:27:29 -0700

    created a home page dart

---
+++ flutter_application_1/lib/pages/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/background.png"),
+                fit : BoxFit.cover,
+                ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
