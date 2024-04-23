08810d61a40641d4fde328938c381e1499f78acf
KleinS <freaKENamazing18@gmail.com>
2024-04-23 10:15:22 -0700

    added location checker function

--- flutter_application_1/lib/models/player_model.dart
+++ flutter_application_1/lib/models/player_model.dart
@@ -1,7 +1,8 @@
 // Define a class for a Player
+import 'package:flutter/foundation.dart';
 import 'package:flutter_application_1/models/deck_model.dart';
 
-class Player {
+class Player extends ChangeNotifier{
   List<CardModel> hand = []; // Hand variable to keep track of the cards in the hand
   final bool isBot; // Boolean variable to tell if the player is a bot or not
   final String name;
@@ -11,25 +12,18 @@ class Player {
   // Function to add a card to the player's hand
   void addCardToHand(CardModel card) {
     hand.add(card);
+    notifyListeners();
   }
 
   // Function to remove a card from the player's hand
   void removeCardFromHand(CardModel card) {
     hand.remove(card);
+    notifyListeners();
   }
 
   // Function to get the number of cards in the player's hand
   int getHandSize() {
     return hand.length;
-  }
-
-  // Function to get the player's hand as a string
-  String getHandAsString() {
-    String handString = "";
-    for (CardModel card in hand) {
-      handString += "${card.value} of ${card.suit}, ";
-    }
-    return handString.substring(0, handString.length - 2);
   }
 
   // Function to remove two cards with the same value from the hand
--- flutter_application_1/lib/pages/options.dart
+++ flutter_application_1/lib/pages/options.dart
@@ -1,31 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Options extends StatelessWidget {
   const Options({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body : Column(
-        children: [
-          Text(
-            "Options Screen \nTemp Text",
-            style: TextStyle(
-              color: Colors.black, 
-              fontSize: 50,
-              fontFamily: 'RedRose',
-            ),
-          ),
-          Text(
-            "Location : ",
-            style: TextStyle(
-              color: Colors.black, 
-              fontSize: 50,
-            ),
-          ),
-        ],
+    return Scaffold(
+      body: FutureBuilder<Position>(
+        future: getUserLocation(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            return Column(
+              children: [
+                const Text(
+                  "Options Screen \nTemp Text",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 50,
+                    fontFamily: 'RedRose',
+                  ),
+                ),
+                Text(
+                  'Location: ${snapshot.data!.latitude}, ${snapshot.data!.longitude}',
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 50,
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
       ),
     );
   }
-    
+}
+
+Future<Position> getUserLocation() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Check if location services are enabled
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+  if (!serviceEnabled) {
+    // Location services are not enabled, show an error message
+    return Future.error('Location services are disabled.');
+  }
+
+
+  // Check if the app has permission to access the user's location
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // The user denied permission, show an error message
+      return Future.error('Location permission denied.');
+    }
+
+  }
+
+  // Get the user's current location
+  return await Geolocator.getCurrentPosition();
 }
