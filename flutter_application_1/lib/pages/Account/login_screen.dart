6ff9359d8face01496dc39fd6b62e518c3315bed
KleinS <freaKENamazing18@gmail.com>
2024-05-01 14:03:00 -0700

    finished with ui for login screen

--- flutter_application_1/lib/pages/Account/login_screen.dart
+++ flutter_application_1/lib/pages/Account/login_screen.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'signup_screen.dart';
+
 class loginScreen extends StatefulWidget {
   const loginScreen({super.key});
 
@@ -10,7 +12,6 @@ class loginScreen extends StatefulWidget {
 }
 
 class _loginScreenState extends State<loginScreen> {
-
   final TextEditingController _passwordTextController = TextEditingController();
   final TextEditingController _emailTextController = TextEditingController();
   @override
@@ -20,43 +21,40 @@ class _loginScreenState extends State<loginScreen> {
         Scaffold(
           body: Container(
             decoration: const BoxDecoration(
-              color:Color.fromRGBO(77, 0, 153, 1),
+              color: Color.fromRGBO(77, 0, 153, 1),
               image: DecorationImage(
                 image: AssetImage("images/login_bg.png"),
                 fit: BoxFit.fill,
               ),
             ),
-
             child: SingleChildScrollView(
               child: Padding(
-                padding: EdgeInsets.fromLTRB(250, MediaQuery.of(context).size.height * 0.25, 250, 30),
+                padding: EdgeInsets.fromLTRB(
+                    250, MediaQuery.of(context).size.height * 0.25, 250, 30),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    
-                    const Padding (
+                  children: [
+                    const Padding(
                       padding: EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        height: 5
-                      ),
+                      child: SizedBox(height: 5),
                     ),
-                    reuseableTextField("Enter Username", Icons.person_outline, false, _emailTextController),
-                
-                    const Padding (
+                    reuseableTextField("Enter Username", Icons.person_outline,
+                        false, _emailTextController),
+                    const Padding(
                       padding: EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        height: 5
-                      ),
+                      child: SizedBox(height: 5),
                     ),
-                    reuseableTextField("Enter Password", Icons.lock_outline, true, _passwordTextController),
-
-                    const Padding (
+                    reuseableTextField("Enter Password", Icons.lock_outline,
+                        true, _passwordTextController),
+                    const Padding(
                       padding: EdgeInsets.all(8.0),
                       child: SizedBox(
                         height: 10,
                       ),
                     ),
-                    LoginSigninButton(context, true, (){}),
+                    LoginSigninButton(context, true, () {}),
+
+                    signUpOption(),
                   ],
                 ),
               ),
@@ -67,51 +65,73 @@ class _loginScreenState extends State<loginScreen> {
     );
   }
 
-  Container LoginSigninButton(BuildContext context, bool isLogin, Function onTap) {
-    return Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 50,
-                    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(90),
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        onTap();
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.resolveWith((states) {
-                          if(states.contains(MaterialState.pressed)) {
-                            return Colors.black;
-                          }
-                          return Colors.white;
-                        }),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
-                        )
-                      ),
-                      child: Text(
-                        isLogin ? 'Login' : 'Sign Up',
-                        style: const TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold, fontSize: 10,
-                        ),
-                      ),
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Dont have account?",
+          style: TextStyle(color: Colors.white),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => const signUpPage()));
+          },
+          child: const Text(
+            " Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
 
-                    ),
-                  );
+  Container LoginSigninButton(
+      BuildContext context, bool isLogin, Function onTap) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(90),
+      ),
+      child: ElevatedButton(
+        onPressed: () {
+          onTap();
+        },
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.black;
+              }
+              return Colors.white;
+            }),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)))),
+        child: Text(
+          isLogin ? 'Login' : 'Sign Up',
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 10,
+          ),
+        ),
+      ),
+    );
   }
 }
 
-TextField reuseableTextField (String text, IconData icon, bool isPasswordType, TextEditingController controller) {
+TextField reuseableTextField(String text, IconData icon, bool isPasswordType,
+    TextEditingController controller) {
   return TextField(
     controller: controller,
     obscureText: isPasswordType,
     enableSuggestions: !isPasswordType,
     autocorrect: !isPasswordType,
     cursorColor: Colors.white,
-    style: TextStyle(
-      color: Colors.white.withOpacity(1)
-    ),
+    style: TextStyle(color: Colors.white.withOpacity(1)),
     decoration: InputDecoration(
       prefixIcon: Icon(
         icon,
@@ -120,6 +140,7 @@ TextField reuseableTextField (String text, IconData icon, bool isPasswordType, T
       labelText: text,
       labelStyle: TextStyle(
         color: Colors.white.withOpacity(0.9),
+        fontSize: 15,
       ),
       filled: true,
       floatingLabelBehavior: FloatingLabelBehavior.never,
@@ -127,9 +148,10 @@ TextField reuseableTextField (String text, IconData icon, bool isPasswordType, T
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(5.0),
         borderSide: const BorderSide(width: 0, style: BorderStyle.none),
-        ),
       ),
-      keyboardType: isPasswordType
-        ? TextInputType.visiblePassword : TextInputType.emailAddress,
-    );
+    ),
+    keyboardType: isPasswordType
+        ? TextInputType.visiblePassword
+        : TextInputType.emailAddress,
+  );
 }
